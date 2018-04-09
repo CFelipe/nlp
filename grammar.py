6caa0757f25592e3568c9312b41578f6f544f53b
Felipe Cortez <felipecortezfi@gmail.com>
2018-04-09 19:42:47 -0300

    Rules now counted and ordered by frequency

--- grammar.py
+++ grammar.py
@@ -1,5 +1,6 @@
 import common
 import argparse
+from collections import Counter
 
 class Node:
     def __init__(self, root, children=None):
@@ -18,13 +19,25 @@ def print_tree(node, level = 0):
         for n in node.children:
             print_tree(n, level + 1)
 
+def save_rule(node, rules):
+    """Saves grammar rules"""
+
+    if node is not None:
+        if node.children:
+            formatted_children = " ".join([node.root for node in node.children])
+            # print("{} -> {}".format(node.root, formatted_children))
+            rules.append("{} -> {}".format(node.root, formatted_children))
+
+        for n in node.children:
+            save_rule(n, rules)
+
 def extract_grammar(filename: str):
     with open(filename, "r") as corpus_file:
         pos_stack = []
         paren_stack = []
         prev = None
         root = None
-        counter = 0
+        rules = []
 
         for line in corpus_file:
             tokens = common.tokenize(line)
@@ -43,14 +56,11 @@ def extract_grammar(filename: str):
                             if el == "(":
                                 break
                     if not paren_stack:
-                        print_tree(root)
-                        print("---")
-                        counter += 1
+                        save_rule(root, rules)
                         pos_stack = []
-                        paren_Stack = []
+                        paren_stack = []
                         prev = None
                         root = None
-                        if counter >= 3: return
                 else:
                     if prev == "(":
                         node = Node(token)
@@ -60,6 +70,9 @@ def extract_grammar(filename: str):
                             root = node
                         pos_stack.append(node)
                 prev = token
+
+        print(len(rules))
+        print(Counter(rules).most_common(30))
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Grammar extractor")
