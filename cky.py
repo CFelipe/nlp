ae7b58afc1aea22ef299f0e1cecbf5085dca01d5
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-03 15:14:46 -0300

    CKY now fixed by handling unit productions

--- cky.py
+++ cky.py
@@ -4,8 +4,10 @@ import pickle
 from grammar import dd, Node, Rule, Grammar, PICKLE_FILE
 
 def print_table(table: list):
-    for idx, lst in enumerate(table):
-        print("{:>2}".format(idx), lst)
+    for i, lst in enumerate(table):
+        for j, cell in enumerate(lst):
+            if i <= j:
+                print("{:>2} {:>2}".format(i, j), cell)
 
 def cky_parse(grammar: Grammar, sentence: list) -> list:
     table = []
@@ -14,19 +16,25 @@ def cky_parse(grammar: Grammar, sentence: list) -> list:
         table.append([set() for _ in range(len(words))])
 
     for j, word in enumerate(words):
-        print(j, word)
+        # print(j, word)
 
         # fill [j, j] cell with RULE -> word
         for rule in grammar.words_pos[word]:
-            print(rule)
+            # print(rule)
             table[j][j].add("{}_NT".format(rule))
-        print_table(table)
-        print("---")
+
+        for rule in grammar.rules:
+            if len(rule.rhs) == 1 and \
+                rule.rhs[0] in table[j][j]:
+                table[j][j].add(rule.lhs)
+        # print_table(table)
+        # print("---")
 
         # fill [j, (i: j..0)] (upwards)
-        print("rev", list(reversed(range(j))))
         for i in reversed(range(j)):
             for k in range(i, j):
+                print("{}-{}: {}-{} + {}-{}".format(i, j, i, k, k + 1, j))
+                print(table[i][k], table[k + 1][j])
                 for rule in grammar.rules:
                     if len(rule.rhs) == 2 and \
                        rule.rhs[0] in table[i][k] and \
@@ -38,6 +46,6 @@ def cky_parse(grammar: Grammar, sentence: list) -> list:
 if __name__ == "__main__":
     with open(PICKLE_FILE, 'rb') as grammar_file:
         grammar = pickle.load(grammar_file)
-        sentence = "Have they been practicing lately ?"
+        sentence = "He smiles ."
 
         cky_parse(grammar, sentence)
