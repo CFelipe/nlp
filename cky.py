2b1a753f1617e3cc392c0c04118cdec0eef5d9fa
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-03 12:20:13 -0300

    CKY apparently working

--- cky.py
+++ cky.py
@@ -4,36 +4,40 @@ import pickle
 from grammar import dd, Node, Rule, Grammar, PICKLE_FILE
 
 def print_table(table: list):
-    for lst in table:
-        print(lst)
+    for idx, lst in enumerate(table):
+        print("{:>2}".format(idx), lst)
 
 def cky_parse(grammar: Grammar, sentence: list) -> list:
     table = []
-    for i in range(len(sentence.split())):
-        table.append([[] for _ in range(i + 1)])
-    print(table)
-
     words = sentence.split()
-    for j, word in enumerate(words, start=1):
-        print(word)
+    for i in range(len(words)):
+        table.append([set() for _ in range(len(words))])
+
+    for j, word in enumerate(words):
+        print(j, word)
+
+        # fill [j, j] cell with RULE -> word
         for rule in grammar.words_pos[word]:
             print(rule)
-            table[j - 1][j - 1].append(rule)
+            table[j][j].add("{}_NT".format(rule))
         print_table(table)
         print("---")
 
-        print(list(reversed(range(j))))
+        # fill [j, (i: j..0)] (upwards)
+        print("rev", list(reversed(range(j))))
         for i in reversed(range(j)):
-
             for k in range(i, j):
-                print(i, k)
-                #for rules -> BC:
-                #    if B in table[i][k - 1] and C in table[k][j - 1]:
-                #        table[i][j - 1].append(rule)
+                for rule in grammar.rules:
+                    if len(rule.rhs) == 2 and \
+                       rule.rhs[0] in table[i][k] and \
+                       rule.rhs[1] in table[k + 1][j]:
+                        table[i][j].add(rule.lhs)
+
+    print_table(table)
 
 if __name__ == "__main__":
     with open(PICKLE_FILE, 'rb') as grammar_file:
         grammar = pickle.load(grammar_file)
-        sentence = "The man jumps"
+        sentence = "Have they been practicing lately ?"
 
         cky_parse(grammar, sentence)
