42d73199e8de170c96eb60b960b2db9285f4a70e
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-04 13:13:45 -0300

    Added command-line args to CKY

--- cky.py
+++ cky.py
@@ -1,6 +1,6 @@
 # 96 quite bitter beings
 
-import pickle
+import pickle, argparse
 from grammar import dd, Node, Rule, Grammar, PICKLE_FILE
 
 def print_table(table: list):
@@ -57,9 +57,45 @@ def cky_parse(grammar: Grammar, sentence: list) -> list:
         sentences = filter(lambda r: r[0] == 'S', table[0][len(words) - 1])
         for sentence in sentences:
             lhs, k, left_idx, right_idx = sentence
-            print(sentence[0])
-            visit(words, table, 0, len(words) - 1, k, left_idx, indent=2)
-            visit(words, table, 0, len(words) - 1, k, right_idx, indent=2)
+            visit(words, table, 0, len(words) - 1, k, left_idx, indent=0)
+            print("---")
+
+def pcky_parse(grammar: Grammar, sentence: list) -> list:
+    table = []
+    words = sentence.split()
+    for i in range(len(words)):
+        table.append([[] for _ in range(len(words))])
+
+    for j, word in enumerate(words):
+        # fill [j, j] cell with rule -> word
+        for rule in grammar.words_pos[word]:
+            table[j][j].append(("{}_NT".format(rule),))
+
+        for rule in grammar.rules:
+            # "flatten" unit productions
+            if len(rule.rhs) == 1 and \
+                rule.rhs[0] in [pos[0] for pos in table[j][j]]:
+                unit_str = "{}->{}".format(rule.lhs, rule.rhs[0])
+                table[j][j].append((unit_str,))
+
+        # fill [j, (i: j..0)] (upwards)
+        for i in reversed(range(j)):
+            for k in range(i, j):
+                # print("{}-{}: {}-{} + {}-{}".format(i, j, i, k, k + 1, j))
+                # print(table[i][k], table[k + 1][j])
+                for rule in grammar.rules:
+                    if len(rule.rhs) == 2 and \
+                       rule.rhs[0] in [pos[0].split("->")[0] for pos in table[i][k]] and \
+                       rule.rhs[1] in [pos[0].split("->")[0] for pos in table[k + 1][j]]:
+                        idx1 = [pos[0].split("->")[0] for pos in table[i][k]].index(rule.rhs[0])
+                        idx2 = [pos[0].split("->")[0] for pos in table[k + 1][j]].index(rule.rhs[1])
+                        table[i][j].append((rule.lhs, k, idx1, idx2))
+
+    if 'S' in [rules[0] for rules in table[0][len(words) - 1]]:
+        sentences = filter(lambda r: r[0] == 'S', table[0][len(words) - 1])
+        for sentence in sentences:
+            lhs, k, left_idx, right_idx = sentence
+            visit(words, table, 0, len(words) - 1, k, left_idx, indent=0)
             print("---")
 
 if __name__ == "__main__":
@@ -67,4 +103,17 @@ if __name__ == "__main__":
         grammar = pickle.load(grammar_file)
         sentence = "This is really rewarding ."
 
-        cky_parse(grammar, sentence)
+        argparser = argparse.ArgumentParser(description="Grammar extractor")
+        argparser.add_argument("sentence", help="Sentence to be parsed")
+        argparser.add_argument(
+            '-p',
+            '--probabilistic',
+            help="Probabilistic CKY",
+            action='store_true')
+
+        args = argparser.parse_args()
+
+        if args.probabilistic:
+            pcky_parse(grammar, args.sentence)
+        else:
+            cky_parse(grammar, args.sentence)
